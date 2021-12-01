dd577f617726a646b1d67c9e726aee1df35e86f3
chickensalt <nsdj.sharma@gmail.com>
2021-12-01 21:25:28 +1100

    Revert "improve day 1 part 2 solution"
    This reverts commit 446e2dd1dbe1bfaee6670e772256371dae01b891.

--- day1/part2.py
+++ day1/part2.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
             if i < len(data) - 2
         ]
         increments = 0
-        for i, _ in enumerate(windows[1:]):
+        for i, _ in enumerate(windows):
+            if i == 0:
+                continue
             if windows[i] > windows[i - 1]:
                 increments += 1
         print(increments)
