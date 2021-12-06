a1768b06eab3f1d2c06c60f348ae49fdaacf525d
chickensalt <nsdj.sharma@gmail.com>
2021-12-06 19:19:19 +1100

    remove collections dependency for 2021 day 6

--- 2021/day6/part1.py
+++ 2021/day6/part1.py
@@ -1,8 +1,7 @@
-from collections import Counter
 from common import simulateFish
 
 if __name__ == "__main__":
     with open("./input.txt", "r") as inputFile:
         fishes = [int(timer) for timer in inputFile.readline().strip().split(",")]
-        fishCounter = dict(Counter(fishes))
+        fishCounter = {i: fishes.count(i) for i in range(9)}
         print(simulateFish(fishCounter, 80))
